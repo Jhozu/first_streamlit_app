2ccd78f17368487963a4940228ac90818e8b1e27
Jhozu <jose3jimenez2000@hotmail.com>
2022-09-24 21:35:03 -0600

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-fruits_selected = streamlit.multiselect("pick some fruits: ", list(my_fruit_list.index),['Avocado','strawberries'])
+fruits_selected = streamlit.multiselect("pick some fruits: ", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
